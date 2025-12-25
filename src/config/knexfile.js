79b3fe50e77d07690339765a50c77f0a548d6959
Muhammad Nursyahid Al Faizi <nurfaizitamam@gmail.com>
2025-12-25 20:22:24 +0700

    mematikan ssl

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -43,7 +43,12 @@ module.exports = {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD, 
-      database: process.env.DB_NAME
+      database: process.env.DB_NAME,
+
+      ssl: {
+        rejectUnauthorized: true, 
+        minVersion: 'TLSv1.2'
+      }
     },
     pool: {
       min: 2,
