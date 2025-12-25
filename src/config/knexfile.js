d7467c5b55f8143fc625e3ee79dcfffb4c30ddac
Muhammad Nursyahid Al Faizi <nurfaizitamam@gmail.com>
2025-12-25 20:33:13 +0700

    coba ditambahkan di development

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -12,7 +12,12 @@ module.exports = {
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD, 
-      database: process.env.DB_NAME
+      database: process.env.DB_NAME,
+      
+      ssl: {
+        minVersion: 'TLSv1.2',
+        rejectUnauthorized: true
+    }
     },
     migrations: {
       tableName: 'knex_migrations',
