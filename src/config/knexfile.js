5a1480b20d5a1d06656b77852bc1dcce968d854a
Muhammad Nursyahid Al Faizi <nurfaizitamam@gmail.com>
2025-12-25 20:05:11 +0700

    add script to migrate in render

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -37,18 +37,21 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'mysql2',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD, 
+      database: process.env.DB_NAME
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './migrations'
     }
   }
 
